48615d72e44a03376c64b23c5957ba442f7bdc2a
Luis Bryan Oroxón Ramíre <lanzer2002@hotmail.com>
2017-06-16 07:25:19 -0500

    Commit

---
+++ AutenticacionXamarin/AutenticacionXamarin/Model/EntityData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft.WindowsAzure.MobileServices;
+
+
+namespace AutenticacionXamarin
+{
+    public class EntityData
+    {
+        public EntityData()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        [CreatedAt]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [UpdatedAt]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [Version]
+        public string AzureVersion { get; set; }
+    }
+}
---
+++ AutenticacionXamarin/AutenticacionXamarin/Services/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AutenticacionXamarin
+{
+    public interface IDataService
+    {
+        Task AddExpenseAsync(Expense ex);
+        Task<IEnumerable<Expense>> GetExpensesAsync();
+    }
+}
